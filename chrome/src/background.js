bda08669fd85b4e56e9a1521391014edb942f22e
Z1VK3 <Z1VK3@users.noreply.github.com>
2024-01-26 16:40:32 +0200

    version bump - move to Semrushbot

--- chrome/src/background.js
+++ chrome/src/background.js
@@ -1,6 +1,5 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function(request) {
-
     // Removing non-working UAs as of Jan 2024
 /*        var newUAs = [
             'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)',
@@ -11,7 +10,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
         var newUA = newUAs[~~(Math.random() * newUAs.length)];
 */
 
-    // Using SemrushBot now
+        // Using SemrushBot now
         var newUA = 'Mozilla/5.0 (compatible; SemrushBot; +http://www.semrush.com/bot.html)';
         var gotUA = false;
         for (var i = 0; i < request.requestHeaders.length; ++i) {
