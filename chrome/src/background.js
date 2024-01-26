1b7d4158f170a419288a8c220dcaf06a063285d0
Z1VK3 <Z1VK3@users.noreply.github.com>
2024-01-26 16:34:36 +0200

    Changing UA to SemrushBot

--- chrome/src/background.js
+++ chrome/src/background.js
@@ -1,12 +1,18 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function(request) {
-        var newUAs = [
+
+    // Removing non-working UAs as of Jan 2024
+/*        var newUAs = [
             'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)',
             'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)',
             'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
             'Twitterbot/1.0'
         ];
         var newUA = newUAs[~~(Math.random() * newUAs.length)];
+*/
+
+    // Using SemrushBot now
+        var newUA = 'Mozilla/5.0 (compatible; SemrushBot; +http://www.semrush.com/bot.html)';
         var gotUA = false;
         for (var i = 0; i < request.requestHeaders.length; ++i) {
             if (request.requestHeaders[i].name == 'User-Agent') {
